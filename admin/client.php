67efbdf3780d94f98a837077f0d767ca89f88028
AjiRamdaniARM <hilmankeren0936@gmail.com>
2023-12-26 10:06:17 +0700

    'push'

--- admin/client.php
+++ admin/client.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
-$query = mysqli_query($conn, "SELECT * FROM tb_client");
+$query = mysqli_query($conn, "SELECT * FROM tb_client WHERE 1");
 while ($record = mysqli_fetch_array($query)) {
     $result[] = $record;
 }
@@ -98,7 +98,7 @@ while ($record = mysqli_fetch_array($query)) {
             foreach ($result as $row ) {
             ?>
 <!-- edit client -->
-<div id="default-modal-edit<?php echo['id_client'] ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="default-modal-edit<?php echo $row['id_client'] ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-2xl max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -132,35 +132,30 @@ while ($record = mysqli_fetch_array($query)) {
     </div>
 </div>
 
-<!-- delete client -->
 
-<div id="ModalDelete<?php echo $row['id_client'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" class="ModalDelete hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<!-- delete modal -->
+<div id="popup-modal<?php echo $row['id_client'] ?>" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="ModalDelete">
+            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal<?php echo $row['id_client'] ?>">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
-                <span class="sr-only">Close modal</span>
+                <span class="sr-only">Close Delete</span>
             </button>
             <div class="p-4 md:p-5 text-center">
                 <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
-                <form action="./config/delete.php">
-                <input type="hidden" value="<?php echo $row['id_client'] ?>" name="id_client">
-               
-                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Apakah kamu yakin ingin menghapus data <?php echo $row['nama_client'] ?></h3>
-                <button data-modal-hide="ModalDelete" type="submit" name="delete" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
-                    Yes, I'm sure
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Kamu Yakin ingin menghapus data <?php echo $row['nama_client']  ?></h3>
+                <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
+                    Iya, saya yakin
                 </button>
-                <button data-modal-hide="ModalDelete type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+                <button data-modal-hide="popup-modal<?php echo $row['id_client'] ?>" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Tidak</button>
             </div>
-            </form>
         </div>
     </div>
 </div>
-
 
 
 
@@ -213,8 +208,8 @@ while ($record = mysqli_fetch_array($query)) {
               <td class="whitespace-nowrap px-6 py-4"><img src="https://drive.google.com/uc?export=view&id=<?php echo $row['id_poto'] ?>" alt="Arsitek"></td>
               <td class="whitespace-nowrap px-6 py-4"><?php echo $row['video'] ?></td>
               <td class="whitespace-nowrap px-6 py-4">
-                <button type="button" data-modal-target="#default-modal-edit<?php echo['id_client'] ?>"  data-modal-toggle="default-modal-edit" class="bg-yellow-500 px-2 py-2 rounded-full font-bold">Edit</button>
-                <button  data-target="#ModalDelete<?php echo $row['id_client'] ?>" data-modal-toggle="ModalDelete" class="bg-red-500 px-2 py-2 rounded-full font-bold">Delete</button>
+                <button type="button" data-modal-target="#default-modal-edit<?php echo $row['id_client'] ?>"  data-modal-toggle="default-modal-edit" class="bg-yellow-500 px-2 py-2 rounded-full font-bold">Edit</button>
+                <button  data-modal-target="popup-modal<?php echo $row['id_client'] ?>" data-modal-toggle="popup-modal<?php echo $row['id_client'] ?>" class="bg-red-500 px-2 py-2 rounded-full font-bold" type="button">Delete</button>
                 
               </td>
             </tr>
